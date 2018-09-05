e95709f3b8331f7cf6ec9b0487e6db785b68fd86
Si Kim Yeung <100072290@students.swinburne.edu.my>
2018-09-05 20:43:53 +0800

    Initial look of new sidebar menu plus updated db
    The sidebar i made is what ive found on the internet, you can change if you have better one.
    
    I also updated the db with a table that i forgot to add in

---
+++ application/views/lmoproject_view.php
@@ -0,0 +1,70 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>New Application - Living Modified Organisms</title>
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url()?>assets/css/simple-sidebar.css" type="text/css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>Bootstrap Sidebar</h3>
+            </div>
+            
+            <ul class="list-unstyled components">
+            <p>Dummy Heading</p>
+            <li class="active">
+                <a href="#">Living Modified Organisms</a>
+            </li>
+            <li>
+                <a href="#">Annex 2</a>
+            </li>
+            <li>
+                <a href="#">Form E</a>
+            </li>
+            <li>
+                <a href="#">PC1</a>
+            </li>
+            <li>
+                <a href="#">PC2</a>
+            </li>
+            <li>
+                <a href="#">OHS - HIRARC</a>
+            </li>
+            <li>
+                <a href="#">Safe Work Procedure</a>
+            </li>
+        </ul>
+        </nav>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content Insert Here -->
+        <div id="content">
+                <div class="container-fluid">
+                </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+
+</body>
+
+</html>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -12,9 +12,20 @@
     <!-- FontAwesome -->
     <!-- Old v4.7: <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" /> -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+    
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
 
     <!-- JQuery - required for Bootstraps JavaScript plugins) -->
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    
     <!-- All Bootstrap plug-ins file -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     
