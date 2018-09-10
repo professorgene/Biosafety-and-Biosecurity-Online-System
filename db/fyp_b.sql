09064f4757af9d8c4364368745835e0ee860f305
professorgene <werewolf.xeno@yahoo.com>
2018-09-10 20:02:07 +0800

    missing sql lines

--- db/fyp_b.sql
+++ db/fyp_b.sql
@@ -22,6 +22,9 @@ SET time_zone = "+00:00";
 -- Database: `fyp_b`
 --
 
+CREATE DATABASE IF NOT EXISTS biosafety_and_biosecurity_online_system;
+USE biosafety_and_biosecurity_online_system;
+
 -- --------------------------------------------------------
 
 --
