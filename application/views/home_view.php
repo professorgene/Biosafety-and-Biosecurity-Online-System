b8379c7a2e84f98efdc04979ba728e3ad4205846
professorgene <werewolf.xeno@yahoo.com>
2018-09-17 10:00:12 +0800

    changed home view

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -47,7 +47,8 @@ if(!$this->session->userdata('isLogin')){
 			</div>
         </h2>
 			<br>
-
+        
+        <!--
         <div class="row">
             <?php if($this->session->userdata('account_type') != 5 && $this->session->userdata('account_type') != 6 && $this->session->userdata('account_type') != 7) { ?>
             <div class="col-lg-3 col-md-4 col-sm-6 col-6 portfolio-item">
@@ -135,6 +136,79 @@ if(!$this->session->userdata('isLogin')){
                     <div style="text-align: center" class="card-body">
                         <h6 class="card-title">
                             <a href="<?php echo base_url(); ?>index.php/history">Submitted and Approved Applications</a>
+                        </h6>
+                        <p class="card-text"></p>
+                    </div>
+                </div>
+            </div>
+            <?php } ?>
+        </div>
+  -->     
+        
+        <div class="row">
+            <?php if($this->session->userdata('account_type') != 5 && $this->session->userdata('account_type') != 6 && $this->session->userdata('account_type') != 7) { ?>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-6 portfolio-item d-flex align-items-stretch">
+                <div class="card card-block justify-content-center align-items-center">
+                    <a href="<?php echo base_url(); ?>index.php/history"><img class="card-img-top" src="<?php echo base_url('assets\images\ApplicantForm\Annual Report.jpg') ?>" alt=""></a>
+                    <div style="text-align: center" class="card-body">
+                        <h6 class="card-title">
+                            <a href="<?php echo base_url(); ?>index.php/history">New Project</a>
+                        </h6>
+                        <p class="card-text"></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-6 portfolio-item d-flex align-items-stretch">
+                <div class="card card-block justify-content-center align-items-center">
+                    <a href="<?php echo base_url(); ?>index.php/history"><img class="card-img-top" src="<?php echo base_url('assets\images\ApplicantForm\History.jpg') ?>" alt=""></a>
+                    <div style="text-align: center" class="card-body">
+                        <h6 class="card-title">
+                            <a href="<?php echo base_url(); ?>index.php/history">Submitted &amp; Approved Projects</a>
+                        </h6>
+                        <p class="card-text"></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-6 portfolio-item d-flex align-items-stretch">
+                <div class="card card-block justify-content-center align-items-center">
+                    <a href="<?php echo base_url(); ?>index.php/inventory/index"><img class="card-img-top" src="<?php echo base_url('assets\images\ApplicantForm\LMO Biohazardous Mat.jpg') ?>" alt=""></a>
+                    <div style="text-align: center" class="card-body">
+                        <h6 class="card-title">
+                            <a href="<?php echo base_url(); ?>index.php/inventory/index">LMO and Biohazardous Material Database</a>
+                        </h6>
+                        <p class="card-text"></p>
+                    </div>
+                </div>
+            </div>
+            <?php } else { ?>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-6 portfolio-item d-flex align-items-stretch">
+                <div class="card card-block justify-content-center align-items-center">
+                    <a href="<?php echo base_url(); ?>index.php/history"><img class="card-img-top" src="<?php echo base_url('assets\images\ApplicantForm\Annual Report.jpg') ?>" alt=""></a>
+                    <div style="text-align: center" class="card-body">
+                        <h6 class="card-title">
+                            <a href="<?php echo base_url(); ?>index.php/history">New Project</a>
+                        </h6>
+                        <p class="card-text"></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-6 portfolio-item d-flex align-items-stretch">
+                <div class="card card-block justify-content-center align-items-center">
+                    <a href="<?php echo base_url(); ?>index.php/history"><img class="card-img-top" src="<?php echo base_url('assets\images\ApplicantForm\History.jpg') ?>" alt=""></a>
+                    <div style="text-align: center" class="card-body">
+                        <h6 class="card-title">
+                            <a href="<?php echo base_url(); ?>index.php/history">Submitted &amp; Approved Projects</a>
+                        </h6>
+                        <p class="card-text"></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-6 portfolio-item d-flex align-items-stretch">
+                <div class="card card-block justify-content-center align-items-center">
+                    <a href="<?php echo base_url(); ?>index.php/inventory/index"><img class="card-img-top" src="<?php echo base_url('assets\images\ApplicantForm\LMO Biohazardous Mat.jpg') ?>" alt=""></a>
+                    <div style="text-align: center" class="card-body">
+                        <h6 class="card-title">
+                            <a href="<?php echo base_url(); ?>index.php/inventory/index">LMO and Biohazardous Material Database</a>
                         </h6>
                         <p class="card-text"></p>
                     </div>
