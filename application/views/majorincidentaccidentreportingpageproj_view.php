40660597f19cb16b46da776288d4ada2cb7abe3c
Jasper753 <ianjasper753@gmail.com>
2018-09-20 21:22:13 +0800

    LMO (Major bio Incident accident form)
    updated pages

---
+++ application/views/majorincidentaccidentreportingpageproj_view.php
@@ -0,0 +1,109 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+if(!$this->session->userdata('isLogin')){
+    redirect('landing/index');
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/styles.css" type="text/css">
+    <title>Biosafety and Biosecurity Online System - New Application: Exempt Dealing Project.</title>
+    
+    <style>
+        body {
+            padding-top: 82px;
+        }
+        
+        .btn-sample{
+            position: fixed;
+            margin-left: 60px;
+        }
+        
+        #first-table{
+            background-color: #95a5a6;
+            text-align: center;
+        }
+        
+        .tblTitle{
+            background-color: black;
+            color: white;
+            text-align: center;
+        }
+        
+        .blackborder{
+            border-color: black;
+        }
+        
+        .approve_section{
+            display: none;
+        }
+        
+        .tblTitle2{
+            background-color: #808080;
+            color: white;
+            text-align: center;
+        }
+        
+        .tbheader1{
+            background-color:  #95a5a6 ;
+        }
+        
+        .centering{
+            text-align: center;
+        }
+        
+        .greendata{
+            background-color: lawngreen;
+        }
+        
+        .reddata{
+            background-color: red;
+        }
+        
+        .yellowdata{
+            background-color: yellow;
+        }
+        
+        .colspace{
+            width: 50px;
+        }
+        
+        .sectiontarget::before {
+          content:"";
+          display:block;
+          height:60px; /* fixed header height*/
+          margin:-60px 0 0; /* negative fixed header height */
+        }
+    </style> 
+</head>    
+<body>
+    <?php include_once 'template/navbar.php' ?>
+    
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <ul class="nav nav-tabs">
+                    <li class="nav-item active"><a href="#annex3tab" class="nav-link" data-toggle="tab">Exempt Dealings Form</a></li>
+                    <li class="nav-item"><a href="#ohstab" class="nav-link" data-toggle="tab">HIRARC Form</a></li>
+                    <li class="nav-item"><a href="#swptab" class="nav-link" data-toggle="tab">Safe Work Procedure</a></li>
+                </ul>
+                
+                <div class="tab-content">
+                    <div class="tab-pane active" id="annex3tab">
+                        <br/>
+                        <?php include 'annex3_view.php' ?>
+                    </div>
+                    <div class="tab-pane fade" id="ohstab">
+                        <br/>
+                        <?php include 'incidentaccidentreport_view.php' ?>
+                    </div>
+                    
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</body>
+</html>
