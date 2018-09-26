88dad564d288262c79205acccf1f15c2beab7405
professorgene <werewolf.xeno@yahoo.com>
2018-09-26 22:19:46 +0800

    updated project model

--- application/models/project_model.php
+++ application/models/project_model.php
@@ -13,77 +13,50 @@ class project_model extends CI_Model
 		return $this->db->insert('project', $data);
 	}
     
-    /*
-	function get_account($email, $pass)
-	{
-		$this->db->where('account_email', $email);
-		$this->db->where('account_password', $pass);
-        $this->db->where('account_approved', 1);
-        $query = $this->db->get('accounts');
-		return $query->result();
-	}
-	
-	# Retrieves Account by ID
-	function get_account_by_id($id)
-	{
-		$this->db->where('account_id', $id);
-        $query = $this->db->get('accounts');
-		return $query->result();
-	}
-    
-    function recover_account($email)
+    function get_proj_id($id)
     {
-        $this->db->where('account_email', $email);
-        $this->db->where('account_approved', 1);
-        $query = $this->db->get('accounts');
-        return $query->result();
-    }
-    
-    function get_account_type($id)
-    {
-        $this->db->select('account_type');
-        $this->db->from('accounts');
-		$this->db->where('account_id', $id);
-        $query = $this->db->get();
-        return $query->result();
-    }
-    
-    function get_all_account() 
-    {
-        $this->db->where('account_approved', 0);
-        $query = $this->db->select('*')->from('accounts')->get();
+        $this->db->where('project_id', $id);
+        $query = $this->db->get('project');
 		return $query->result();
     }
     
-    function update_account($id, $data)
+    function get_proj_by_user_id($id)
+    {
+        $this->db->where('account_id', $id);
+        $query = $this->db->get('project');
+		return $query->result();
+    }
+    
+    function get_all_proj() 
+    {
+        $this->db->where('project_approval', 0);
+        $query = $this->db->select('*')->from('project')->get();
+		return $query->result();
+    }
+    
+    function update_proj($id, $data)
     {
         extract($data);
-        #if ($account_email != ''){
-        #    $this->db->where('account_id', $id);
-        #    $this->db->update('accounts', array('account_email' => $account_email));
-        #}
         if ($account_password != ''){
-            $this->db->where('account_id', $id);
-            $this->db->update('accounts', array('account_password' => $account_password));
+            $this->db->where('project_id', $id);
+            $this->db->update('project', array('project_name' => $project_name, 'project_desc' => $project_desc));
         }
-        #return $this->db->last_query();
         return true;
     }
     
-    function update_approval($id, $type)
+    function update_proj_approval($id, $type)
     {
         if ($type == 0) {
             #$data = array('account_approved' => 0);
-            $data = array('account_email' => 'denied', 'account_approved' => 2);
-            $this->db->where('account_id', $id);
-            $this->db->update('accounts', $data);
+            $data = array('project_approval' => 2);
+            $this->db->where('project_id', $id);
+            $this->db->update('project', $data);
         } elseif ($type == 1) {
-            $data = array('account_approved' => 1);
-            $this->db->where('account_id', $id);
-            $this->db->update('accounts', $data);
+            $data = array('project_approval' => 1);
+            $this->db->where('project_id', $id);
+            $this->db->update('project', $data);
         }
         return true;
     }
-    */
 }
 ?>
