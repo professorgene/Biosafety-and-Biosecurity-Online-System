61f4d6c665fdc08c21a0f0b124c5ab9ff3cfaf88
professorgene <werewolf.xeno@yahoo.com>
2018-10-14 17:30:28 +0800

    statistic module completed
    add more statistics as required into the template

--- assets/js/simple-donut-jquery.js
+++ assets/js/simple-donut-jquery.js
@@ -5,7 +5,7 @@
  * @param  {number}  percent Passing in 22.3 will make the chart show 22%
  * @param  {boolean} donut   True shows donut, false shows pie
  */
-function updateDonutChart (el, percent, donut) {
+function updateDonutChart (el, percent) {
     percent = Math.round(percent);
     if (percent > 100) {
         percent = 100;
@@ -21,15 +21,30 @@ function updateDonutChart (el, percent, donut) {
         $(el + ' .pie').css('clip', 'rect(0, 1em, 1em, 0.5em)');
         $(el + ' .right-side').css('transform', 'rotate(0deg)');
     }
-    if (donut) {
-        $(el + ' .right-side').css('border-width', '0.1em');
-        $(el + ' .left-side').css('border-width', '0.1em');
-        $(el + ' .shadow').css('border-width', '0.1em');
-    } else {
-        $(el + ' .right-side').css('border-width', '0.5em');
-        $(el + ' .left-side').css('border-width', '0.5em');
-        $(el + ' .shadow').css('border-width', '0.5em');
-    }
+    
+    $(el + ' .right-side').css('border-width', '0.1em');
+    $(el + ' .left-side').css('border-width', '0.1em');
+    $(el + ' .shadow').css('border-width', '0.1em');
+        
     $(el + ' .num').text(percent);
+    $(el + ' .left-side').css('transform', 'rotate(' + deg + 'deg)');
+}
+
+function updatePieChart (el, value, total) {
+    var deg = Math.round(360 * (value / total));
+
+    if (Math.round((value / total)) > 50) {
+        $(el + ' .pie').css('clip', 'rect(auto, auto, auto, auto)');
+        $(el + ' .right-side').css('transform', 'rotate(180deg)');
+    } else {
+        $(el + ' .pie').css('clip', 'rect(0, 1em, 1em, 0.5em)');
+        $(el + ' .right-side').css('transform', 'rotate(0deg)');
+    }
+    
+    $(el + ' .right-side').css('border-width', '0.5em');
+    $(el + ' .left-side').css('border-width', '0.5em');
+    $(el + ' .shadow').css('border-width', '0.5em');
+    
+    $(el + ' .num').text(value + ' / ' + total);
     $(el + ' .left-side').css('transform', 'rotate(' + deg + 'deg)');
 }
