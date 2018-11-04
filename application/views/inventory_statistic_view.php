9813980ff89aab4b0c7f6875565c0f29efd7fff7
Jasper753 <ianjasper753@gmail.com>
2018-11-04 15:22:33 +0800

    slide for Labelling Requirements

--- application/views/inventory_statistic_view.php
+++ application/views/inventory_statistic_view.php
@@ -50,9 +50,49 @@ if(!$this->session->userdata('isLogin')){
             </div>
         </div>
         <hr/>
+		
+        <div class="col-lg-12">
+			<div>		
+				<h4>Labelling Requirements</h4>
+			</div>				
+			<div>		
+				<div class="row">
+          <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+			  <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            </ol>
+			<div class="carousel-inner" role="listbox">
+              <div class="carousel-item active">
+                <img class="d-block img-fluid" src="<?php echo base_url('assets\images\6_LabellingRequirements\LR_1.jpg') ?>" alt="First slide">
+			  </div>
+              <div class="carousel-item">
+                <img class="d-block img-fluid" src="<?php echo base_url('assets\images\6_LabellingRequirements\LR_2.jpg') ?>" alt="Second slide">
+			  </div>
+              <div class="carousel-item">
+                <img class="d-block img-fluid" src="<?php echo base_url('assets\images\6_LabellingRequirements\LR_3.jpg') ?>" alt="Third slide">			  
+			  </div>
+			  <div class="carousel-item">
+                <img class="d-block img-fluid" src="<?php echo base_url('assets\images\6_LabellingRequirements\LR_4.jpg') ?>" alt="Forth slide">			  
+			  </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>				
+				</div>
+			</div>
+			
+		</div>
         
-        
-        
+        <br><br>
         
     </div>
 </body>
