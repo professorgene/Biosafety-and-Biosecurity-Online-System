0e9b1ac1ec357ad79316bf9552c828c0a78275d1
Si Kim Yeung <100072290@students.swinburne.edu.my>
2018-11-15 12:56:14 +0800

    load fix

--- application/views/incidentaccident_exempt_view.php
+++ application/views/incidentaccident_exempt_view.php
@@ -92,7 +92,7 @@ if($this->session->userdata('account_type') != 4 && $this->session->userdata('ac
                                             echo "HSO / Lab Officer";
                                         }
                             ?></td>
-                        <td><button type="button" name = 'load' value = 'Load' onclick="location.href='<?php echo site_url().'/incidentaccidentreportingpageexemptproj/load_project?id='.$row->project_id;?>'" class="btn btn-primary">Load</button></td>
+                        <td><button type="button" name = 'load' value = 'Load' onclick="load(<?php echo $row->project_id; ?>)" class="btn btn-primary">Load</button></td>
                         
                         <td class="text-center">
                             <i class="btn btn-success fa fa-check" onclick="approve(<?php echo $row->account_id; ?>, <?php echo $row->project_id; ?>)" title="Approve"></i>
@@ -150,7 +150,7 @@ if($this->session->userdata('account_type') != 4 && $this->session->userdata('ac
                                             echo "HSO / Lab Officer";
                                         }
                             ?></td>
-                        <td><button type="button" name = 'load' value = 'Load' onclick="location.href='<?php echo site_url().'/incidentaccidentreportingpageexemptproj/load_project?id='.$row->project_id;?>'" class="btn btn-primary">Load</button></td>
+                        <td><button type="button" name = 'load' value = 'Load' onclick="load(<?php echo $row->project_id; ?>)" class="btn btn-primary">Load</button></td>
                         
                         <td class="text-center">
                             <button class="btn btn-success" onclick="approve2(<?php echo $row->account_id; ?>, <?php echo $row->project_id; ?>)" title="Approve"><i class="fa fa-check"></i></button>
@@ -182,6 +182,10 @@ if($this->session->userdata('account_type') != 4 && $this->session->userdata('ac
     </script>
     
     <script>
+        function load(i){
+            window.open("<?php echo base_url(); ?>index.php/incidentaccidentreportingpageexemptproj/load_project/" + i);
+        }
+        
         function approve(i,k){
             
             window.open("<?php echo base_url(); ?>index.php/comment/index/" + i + "/" + k );
